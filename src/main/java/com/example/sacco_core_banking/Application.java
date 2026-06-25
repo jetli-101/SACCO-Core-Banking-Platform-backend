2898b9b5f5cfce3ab1e7f67ff037c3d343eeade1
jeth-consultant <sumbeiywetjethro4@gmail.com>
2026-06-25 21:32:22 +0300

    setting up the sacco core banking backend

---
+++ src/main/java/com/example/sacco_core_banking/Application.java
@@ -0,0 +1,13 @@
+package com.example.sacco_core_banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
